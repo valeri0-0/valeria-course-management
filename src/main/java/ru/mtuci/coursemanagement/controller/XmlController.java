78fb5e77703e0ff46d95c2a2843a7030d1093f4a
valeri0-0 <inspacequeen@gmail.com>
2025-12-25 20:30:40 +0300

    Устранение OWASP A06

--- src/main/java/ru/mtuci/coursemanagement/controller/XmlController.java
+++ src/main/java/ru/mtuci/coursemanagement/controller/XmlController.java
@@ -14,7 +14,14 @@ public class XmlController {
     @PostMapping(value = "/api/xml/parse", consumes = {MediaType.TEXT_XML_VALUE, MediaType.APPLICATION_XML_VALUE})
     public String parse(@RequestBody String xml) throws Exception {
         SAXReader reader = new SAXReader();
-        Document doc = reader.read(new StringReader(xml));
-        return doc.getRootElement().getText();
+
+        reader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        reader.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        reader.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
+        Document document = reader.read(new StringReader(xml));
+
+        return document.getRootElement().getText();
     }
 }
