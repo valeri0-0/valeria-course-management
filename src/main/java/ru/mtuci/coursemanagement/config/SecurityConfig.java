a639c8e0208644ca6c60ed418a9bdc482503348f
valeri0-0 <inspacequeen@gmail.com>
2025-12-25 20:09:28 +0300

    Устранение OWASP A01

---
+++ src/main/java/ru/mtuci/coursemanagement/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package ru.mtuci.coursemanagement.config;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http.csrf(csrf -> {});
+        return http.build();
+    }
+}
+
+
+
